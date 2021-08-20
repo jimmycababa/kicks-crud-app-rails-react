5be6e9a6a1ba845ee042b042f30c7cf66cbafe90
jimmycababa <jimmycababa@gmail.com>
2021-08-20 10:01:55 -0500

    created api controller folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :kicks
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :kicks, only: [:index, :show, :create, :edit, :destroy]
 end
+
+# root 'kicks#index'
+
