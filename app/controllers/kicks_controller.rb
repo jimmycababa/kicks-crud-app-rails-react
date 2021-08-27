95d9497a5266d0a83182fb8f3463240e4a3c1a9b
jimmycababa <jimmycababa@gmail.com>
2021-08-27 12:20:10 -0500

    got rails routing working for show and edit

--- app/controllers/kicks_controller.rb
+++ app/controllers/kicks_controller.rb
@@ -1,4 +1,19 @@
 class KicksController < ApplicationController
  def index
- end 
+  @kicks = Kick.all
+ end
+
+  def show
+    shoe = Kick.find(params[:id])
+      if shoe
+
+        render json: shoe
+      else
+        render json: shoe.errors
+      end
+  end
+
+  def edit
+    @kick = Kick.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
+  # namespace :api do
+  #   namespace :v1 do
   resources :kicks, only: [:index, :show, :create, :edit, :destroy]
-end
-end
+
 
 root 'kicks#index'
 # get '*path', to: 'kicks#index'
